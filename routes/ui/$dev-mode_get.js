85d82329750df36e681902972b7e9a01e4c3d047
Stas Buldakov <xamelon96@gmail.com>
2023-12-27 19:47:17 +0400

    Add debug overlay to show model

--- routes/ui/$dev-mode_get.js
+++ routes/ui/$dev-mode_get.js
@@ -32,3 +32,83 @@ function waitForUpdate() {
     })
 }
 waitForUpdate();
+
+function tag(name, ...children) {
+    const result = document.createElement(name);
+    for (const child of children) {
+        if (typeof(child) === 'string') {
+            result.appendChild(document.createTextNode(child));
+        } else {
+            result.appendChild(child);
+        }
+    }
+
+    result.att$ = function(name, value) {
+        this.setAttribute(name, value);
+        return this;
+    };
+
+    result.onclick$ = function(callback) {
+        this.onclick = callback;
+        return this;
+    };
+
+    return result;
+}
+
+async function getDebugModel() {
+    var resp = await fetch(window.location.href, {
+	headers: {
+	    "x-aidbox-dev": "model"
+	}
+    });
+    var json = await resp.json();
+
+    return json;
+}
+
+async function showDebugOverlay() {
+
+
+    var root = tag('div')
+	.att$("style", `position: absolute;
+bottom: 10; right: 10; top: 10; left: 10;
+background-color: rgba(255,255,255,0.95);`)
+	.att$("id", "debug-overlay");
+
+    var header = tag('div',
+		     tag('h1', "Debug Overlay"),
+		     tag('button', 'CLOSE').onclick$(() => root.remove()),
+		    )
+	.att$("style", `
+display: flex; width: 100%;
+justify-content: space-between;
+background-color: rgba(124,0,124,0.3);
+padding: 10px 10px;`);
+    root.appendChild(header);
+
+    var modelPreview = tag("pre").att$("id", "model");
+    root.appendChild(modelPreview);
+
+    document.body.insertAdjacentElement("beforeend", root);
+
+    var model = await getDebugModel();
+    console.log("MODEL: ", model);
+    modelPreview.innerHTML = JSON.stringify(model, null, 2);
+}
+
+function debugButton() {
+    var button = document.createElement("button");
+
+    button.onclick = () => showDebugOverlay();
+    button.innerHTML = "DEBUG"
+    button.style.cssText = `
+position: absolute;
+bottom: 0; right: 0;
+background-color: rgba(124,0,124,0.3);
+padding: 5px 10px`;
+
+    return button;
+}
+
+document.body.insertAdjacentElement("beforeend", debugButton());
