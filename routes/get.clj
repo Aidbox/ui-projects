fd356347f128bbdf1ce007011ff099755d02403f
Vlad Ganshin <ganshinv@gmail.com>
2023-12-28 16:13:12 +0100

    root page

--- routes/get.clj
+++ routes/get.clj
@@ -1,8 +1,17 @@
 [:html
- [:h1 "UI operations"]
+ [:h1 "Aidbox UI projects"]
+
+
+ [:ul
+  [:li [:a {:href "/ui/auth/userifno"} "Auth UI"]]
+  [:li [:a {:href "/ui/patients"} "Patient CRUD"]]
+  [:li [:a {:href "/ui/chat"} "Chat App"]]
+  [:li [:a {:href "/ui/forms/responses"} "Aidbox Forms"]]]
+
 
  (for [op (map :resource (box/sql "select * from operation where id like 'aidbox-%'"))]
    [:div
     (str (:request op))])
 
  ]
+
